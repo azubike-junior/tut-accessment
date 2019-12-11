97abca63938794f1cd438a7f789af3d9dd0ce1c2
zubi <azu@gmail.com>
2019-12-11 01:05:56 +0100

    new change

--- main.js
+++ main.js
@@ -1,48 +1,24 @@
 // write the code to sum all
 let salaries = {
-    john: 100,
-    Ann: 160,
-    Pete: 130
-}
-
-let sum = 0
-for (let key in salaries) {
-    sum += salaries[key]
-}
-// console.log(sum)
+	john: 100,
+	Ann: 160,
+	Pete: 130
+};
+console.log(Object.values(salaries).reduce((sum, salary) => (sum += salary), 0));
 
 // create a function multiplyNumeric(obj)
-let menu = {
-    width: 200,
-    height: 300,
-    title: "my menu"
+let menus = {
+	width: 200,
+	height: 300,
+	title: 'my menu'
 };
-
-function multiplyNumeric(obj) {
-    for (let key in obj) {
-        if (!isNaN (obj[key])) {
-             console.log(obj[key] *= 2)
-        }
-    }
-}
-multiplyNumeric(menu)
+console.log(Object.values(menus).filter(menu => !isNaN(menu)).map(x => x * 2))
 
 // given an array of Numbers sort number in order, even, then odd
-let arr = [1, 23, 5, 2, 9, 8, 7, 10, 22, 6]
-// console.log(arr.sort((a, b) => a % 2 - b % 2))
-
-
+let arr = [ 1, 23, 5, 2, 9, 8, 7, 10, 22, 6 ];
+console.log(arr.sort((a, b) => a % 2 - b % 2))
 
 //a function that determines a max number
-const data = [ 2, 44, 21, 4, 32, 211, 23, 45]
+const datas = [ 2, 44, 21, 4, 32, 211, 23, 45 ];
+console.log(datas.reduce((a, b) => a > b ? a : b))
 
-function MaxNo(values){
-    let largest = 0;
-    for(let i=0; i<values.length; i = i+1){
-        if(values[i] > largest){
-            largest = values[i]
-        }
-    }
-    return largest
-}
-// console.log(MaxNo(data))
